30b921ff8eb6e77f20350a93ea1970682c1bb075
ibrahmounk <myboxconn@gmail.com>
2022-06-17 23:52:07 +0000

    data types in JS

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-// String
+// TypeOf
 const myName = 'Moncere'
-let myhairColor = 'Black and white'
-
-console.log(myhairColor)
-console.log(myName)
-
-// Boolean
 const right = true
-let left = false
-console.log(right)
-console.log(left)
-
-// Numbers
 let myAge = 24
-console.log(myAge)
-
-// Array
-const friend0 = [38, 13, 24, 67]
-const friend1 = [true, false]
-const friend2 = ['Mamadou', 'Saly']
 const friend3 = [38, true, false, 'Mah']
-
-console.log(friend0)
-console.log(friend1)
-console.log(friend2)
-console.log(friend3)
-console.log(friend3[2])
-
-// Undefined 
 const someValue = undefined
-console.log(someValue)
-
-// BitInt
-
 let big = 246575766666677744422424564646
-console.log(big)
-
 let big1 = 246575766666677744422424564646n
-console.log(big1)
-
-// null
 const whatIsNull = null
-console.log(whatIsNull)
 
-
-// A lot more to discuss later
+const typeOf0 = typeof(myName)
+console.log(typeOf0)
+const typeOf1 = typeof(right)
+console.log(typeOf1)
+const typeOf2 = typeof(myAge)
+console.log(typeOf2)
+const typeOf3 = typeof(friend3)
+console.log(typeOf3)
+const typeOf4 = typeof(someValue)
+console.log(typeOf4)
+const typeOf5 = typeof(big)
+console.log(typeOf5)
+const typeOf6 = typeof(big1)
+console.log(typeOf6)
+const typeOf7 = typeof(whatIsNull)
+console.log(typeOf7)
