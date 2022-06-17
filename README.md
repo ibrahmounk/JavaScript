a04d606687fd9631d83c90fb016bd826cf2f4730
ibrahmounk <myboxconn@gmail.com>
2022-06-17 21:53:30 +0000

    data types in JS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JavaScript
-#Variables
+#Data types
--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-//Difference between Const and let
+// String
+const myName = 'Moncere'
+let myhairColor = 'Black and white'
 
-// Const
-const myFirstVariable = 250;
-console.log(myFirstVariable);
+console.log(myhairColor)
+console.log(myName)
 
-myFirstVariable = 251;
-console.log(myFirstVariable);
+// Boolean
+const right = true
+let left = false
+console.log(right)
+console.log(left)
+
+// Numbers
+let myAge = 24
+console.log(myAge)
+
+// Array
+const friend0 = [38, 13, 24, 67]
+const friend1 = [true, false]
+const friend2 = ['Mamadou', 'Saly']
+const friend3 = [38, true, false, 'Mah']
+
+console.log(friend0)
+console.log(friend1)
+console.log(friend2)
+console.log(friend3)
+console.log(friend3[2])
+
+// Undefined 
+const someValue = undefined
+console.log(someValue)
+
+// BitInt
+
+let big = 246575766666677744422424564646
+console.log(big)
+
+let big1 = 246575766666677744422424564646n
+console.log(big1)
+
+// null
+const whatIsNull = null
+console.log(whatIsNull)
 
 
-
-// Let
-
-let mySecondVariable = 150;
-console.log(mySecondVariable);
-
-mySecondVariable = 100;
-console.log(mySecondVariable);
-// const mySecondVariable = 250;
+// A lot more to discuss later
