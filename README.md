cecafd736fc178220a79b06bdfb42acbdbcbdde0
ibrahmounk <myboxconn@gmail.com>
2022-06-23 04:23:21 +0000

    Call by reference in objects

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JavaScript
-#Objects
+#Call by reference in objects
--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-const myObject = {
-    'key1':{
-        'key3': 'Hello world',
-        key4 : 'Dada world',
-        'key5 & 6': 'Papi world'
-    } ,
-    'key2': 'Bbb',
-    key7: null,
-    key8: undefined,
-    key9: true
+const myProfile = {
+    name: 'Ibrahima',
+    age: 23
 }
 
-console.log(myObject['key1']);
-console.log(myObject['key1']['key4']);
-console.log(myObject.key1['key5 & 6']);
+const teacherProfile = {
+    name: 'Mohamed',
+    age: 55
+}
+
+function primitiveMutate(primitive) {
+    primitive++
+    console.log(primitive)
+}
+let num = 4
+primitiveMutate(num)
+
+///////////////////////////////////////////////
+
+function mutate(obj) {
+    obj.age+++
+    console.log(obj.age)
+}
 
 
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mutate(teacherProfile)
+console.log(teacherProfile)
 
 
 
@@ -56,6 +43,41 @@ console.log(myObject.key1['key5 & 6']);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const myObject = {
+//     'key1':{
+//         'key3': 'Hello world',
+//         key4 : 'Dada world',
+//         'key5 & 6': 'Papi world'
+//     } ,
+//     'key2': 'Bbb',
+//     key7: null,
+//     key8: undefined,
+//     key9: true
+// }
+
+// console.log(myObject['key1']);
+// console.log(myObject['key1']['key4']);
+// console.log(myObject.key1['key5 & 6']);
+
+
+// ////////////////////////////////////////////////////////////////
 
 // const result = 1 === '1'
 // const right = 1 === 1
@@ -82,10 +104,7 @@ console.log(myObject.key1['key5 & 6']);
     // }
     // console.log(breakNumber(10))
 
-
-
-
-
+/////////////////////////////////////////////////////////////////
 
         // function skipNumbers(number) {
 
@@ -106,27 +125,7 @@ console.log(myObject.key1['key5 & 6']);
     // console.log(skipNumbers(20))
     // console.log(skipNumbers(2))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////////
 
 // function skipNumbers(number) {
 //     let evenNumber = []
