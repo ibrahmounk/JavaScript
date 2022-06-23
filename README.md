b6a6e95ab9965a9c9be20e0fb566acf1cfc88ee0
ibrahmounk <myboxconn@gmail.com>
2022-06-23 00:41:37 +0000

    Logical operators

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JavaScript
-#If else
+#Logical operators
--- script.js
+++ script.js
@@ -17,12 +17,40 @@ let sum = 7;
 //         return false
 // }
 
-function getAgege(age) {
-    if(age > 18) {
-        console.log('This person is ' + age + ' years old')
-    }else{
-    console.log('This person is only ' + age + ' years old')
+function getAgage(age, bribe, whitelisted) {
+    if(!whitelisted){
+        console.log('You are banned', whitelisted)
+        return
+    }
+    if((age <= 18 && bribe > 100) || bribe > 500) {
+        console.log('You pass')
+        return
+    
+    } else if (age > 18) {
+        console.log('He is above 18')
+        return
+    }else if (bribe <= 100) {
+        console.log('This person is below 18')
     }
 }
 
-getAgege(18)
+getAgage(100, 100, true)
+// getAgage(100, 100, false)
+
+// getAgage(100, 100, undefined)
+// getAgage(100, 100, null)
+
+
+// getAgage(100, 100, 0)
+
+
+
+
+// else if(age > 18) {
+//     console.log('This person is ' + age + ' years old')
+//     return
+// }else if(bribe > 100 ){
+//     console.log('Congrats')
+// }else{
+//     console.log('This person is only ' + age + ' years old')
+// }
